5c36cccc3f8e1e47ebad0359c380a76c1ced9071
yangyang121 <844171995@qq.com>
2020-06-25 19:57:50 +0800

    feat:Stack

---
+++ Stack.ts
@@ -0,0 +1,45 @@
+class Stack<T> {
+  private topOfStack: number;
+  private items: T[];
+  private EmptyTOS = -1;
+
+  constructor() {
+    this.topOfStack = this.EmptyTOS;
+    this.items = [];
+  }
+
+  isEmpty(): boolean {
+    return this.topOfStack === this.EmptyTOS;
+  }
+
+  makeEmpty() {
+    this.topOfStack = this.EmptyTOS;
+  }
+
+  push(element: T) {
+    this.items[++this.topOfStack] = element;
+  }
+
+  top(): T {
+    if (!this.isEmpty()) return this.items[this.topOfStack];
+    throw new Error("stack is null");
+  }
+
+  pop() {
+    if (!this.isEmpty) this.topOfStack--;
+  }
+
+  topAndPop(): T {
+    if (!this.isEmpty()) return this.items[this.topOfStack--];
+    throw new Error("stack is null");
+  }
+
+  toString(): string {
+    let index = 0;
+    let string = "";
+    while (index <= this.topOfStack) {
+      string += ` ${this.items[index++]}`;
+    }
+    return string;
+  }
+}
